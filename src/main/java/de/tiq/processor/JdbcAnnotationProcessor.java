9f5e35a6fc5db5880109345d5f0e46378930040d
Dan Häberlein <dan.haeberlein@tiq-solutions.de>
2013-01-09 16:28:05 +0100

    finished refactoring of JdbcAnnotaionProcessor class

--- src/main/java/de/tiq/processor/JdbcAnnotationProcessor.java
+++ src/main/java/de/tiq/processor/JdbcAnnotationProcessor.java
@@ -21,7 +21,6 @@ import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -30,12 +29,8 @@ import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
-import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.TypeMirror;
 import javax.tools.Diagnostic.Kind;
 import javax.tools.JavaFileObject;
 
@@ -43,100 +38,120 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 
-import de.tiq.jdbc.annotations.Connection;
-import de.tiq.jdbc.annotations.ConnectionMetaData;
 import de.tiq.jdbc.annotations.JdbcDriver;
+import de.tiq.processor.data.AnnotationElementData;
 import de.tiq.velocity.VelocityController;
 
-@SupportedAnnotationTypes("*")
-@SupportedSourceVersion(SourceVersion.RELEASE_6)
+@SupportedAnnotationTypes("de.tiq.jdbc.annotations.*")
 public class JdbcAnnotationProcessor extends AbstractProcessor{
 
-	private static final int _ANNOTATION_PROCESSOR_NOT_ALREADY_CALLED = 0;
-
 	private static final String DEFAULT_PACKAGE = "de.tiq.jdbc";
-	
-	private int laps;
 	
 	private Messager msgr;
 	private Filer filer;
 	private VelocityController vc;
 
-	private int driverAnnotationCount;
-
 	private String executorClassName;
 	private String connectionHandlerClassName;
 	private String connectionMetaDataProviderClassName;
 	
+	private boolean createdDriverClass;
+	private boolean createdSources;
+	
 	@Override
 	public void init(ProcessingEnvironment processingEnv) {
 		super.init(processingEnv);
-		// annoation processor
 		filer = processingEnv.getFiler();
 		msgr = processingEnv.getMessager();
-		// initialize apache velocity
 		vc = new VelocityController();
 	}
 
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-		if(roundEnv.getRootElements().size() != 0 && laps == _ANNOTATION_PROCESSOR_NOT_ALREADY_CALLED) {
-			Set<? extends Element> driverAnnotatedClass = roundEnv.getElementsAnnotatedWith(JdbcDriver.class);
-			try {
-				processElements(roundEnv);
-				createTemplateClass("QueryExecutor", DEFAULT_PACKAGE, vc.getQueryExecTemp());
-				createTemplateClass("ConnectionHandler", DEFAULT_PACKAGE, vc.getConnectionHandlerTemplate());
-				createTemplateClass("ConnectionMetaDataProvider", DEFAULT_PACKAGE, vc.getConnectionMetaDataProviderTemplate());
-				createTemplateClass("TIQConnection", DEFAULT_PACKAGE, vc.getConnectionTemp());
-				createTemplateClass("TIQStatement", DEFAULT_PACKAGE, vc.getStatementTemp());
-				warnForUsage(driverAnnotatedClass, driverAnnotationCount);
-				msgr.printMessage(Kind.NOTE, "Source generation successfully finished!");
-			} catch (Exception e) {
-				msgr.printMessage(Kind.ERROR, "failed to generate classes, stacktrace was:\n" + e);
-			}
+		msgr.printMessage(Kind.NOTE, "Started Annotation Processing, Using JDBC_Annotation Version 0.02");
+		boolean classesConsumed = true;
+		AnnotationElementData elementData = new AnnotationElementData(roundEnv);
+		boolean processClassesInThatRound = willProcessingClasses(roundEnv, elementData);
+		if(processClassesInThatRound){
+			processAnnotatedElements(elementData);
+		} else {
+			classesConsumed = false;
 		}
-		laps++;
-		// annotations consumed
-		return true;
+		return classesConsumed;
+	}
+	
+	private boolean willProcessingClasses(RoundEnvironment roundEnv, AnnotationElementData elementData){
+		boolean classesAvailable = roundEnv.getRootElements().size() != 0;
+		int sumOfAnnotationOccurence = elementData.getDriverAnnotatedElements().size() 
+									 + elementData.getConnectionAnnotatedElements().size()
+									 + elementData.getConMetaDataAnnotatedElements().size();
+		boolean annotatedClassesForThisProcessorArePresent = sumOfAnnotationOccurence != 0;
+		return classesAvailable && annotatedClassesForThisProcessorArePresent;
 	}
 
-	private void processElements(RoundEnvironment roundEnv) throws IOException {
-		for (Element curElement : roundEnv.getRootElements()) {
-			if (curElement.getAnnotation(javax.annotation.Generated.class) == null && curElement.getKind() == ElementKind.CLASS) {
-				Connection con = curElement.getAnnotation(Connection.class);
-				JdbcDriver driverAnnotation = curElement.getAnnotation(JdbcDriver.class);
-				ConnectionMetaData conMetaData = curElement.getAnnotation(ConnectionMetaData.class);
-				TypeElement classElement = (TypeElement)curElement;
-				if (con != null) {
-					if (checkExecutorClass(classElement.getSuperclass(), "ConnectionHandler")) {
-						connectionHandlerClassName = curElement.toString();
-					} else {
-						connectionHandlerClassName = null;
-						msgr.printMessage(Kind.WARNING, "The class " + curElement.toString() + " need to extend the abstract class \"ConnectionHandler\"!");
-					}
-				}
-				if(conMetaData != null){
-					List<? extends TypeMirror> containedInterfaces = classElement.getInterfaces();
-					if (foreachToStringEndsWith(containedInterfaces.toArray(new TypeMirror[containedInterfaces.size()]), "ConnectionMetaDataProvider")) {
-						connectionMetaDataProviderClassName = curElement.toString();
-					} else {
-						connectionMetaDataProviderClassName = null;
-						msgr.printMessage(Kind.WARNING, "The class " + curElement.toString() + " need to implement the interface \"ConnectionMetaDataProvider\"!");
-					}
-				}
-				if(driverAnnotation != null){
-					if (checkExecutorClass(classElement.getSuperclass(), "QueryExecutor")) {
-						executorClassName = curElement.toString();
-					} else {
-						executorClassName = null;
-						msgr.printMessage(Kind.WARNING, "The class " + curElement.toString() + " need to extend the abstract class \"QueryExecutor\"!");
-					}
-					createDriverClass(evaluatePackage(driverAnnotation.packageDefinition(), curElement),
-													  driverAnnotation.name(), 
-													  driverAnnotation.prefix(),
-													  driverAnnotation.scheme());
-				}
-			}
+	private void processAnnotatedElements(AnnotationElementData elementData) {
+		try {
+			evalDriverAnnotatedElements(elementData.getDriverAnnotatedElements());
+			getTypeNameOfConnectionAnnotatedElement(elementData.getConnectionAnnotatedElements());
+			getTypeNameOfConnectionMetaDataAnnotatedElement(elementData.getConMetaDataAnnotatedElements());
+			createDefaultTemplateSources();
+		} catch (Exception e) {
+			msgr.printMessage(Kind.ERROR, "failed to generate classes, stacktrace was:\n");
+			e.printStackTrace();
+		}
+	}
+
+	private void createDefaultTemplateSources() throws IOException {
+		if (!createdSources) {
+			createTemplateClass("QueryExecutor", DEFAULT_PACKAGE, vc.getQueryExecTemp());
+			createTemplateClass("ConnectionHandler", DEFAULT_PACKAGE, vc.getConnectionHandlerTemplate());
+			createTemplateClass("ConnectionMetaDataProvider", DEFAULT_PACKAGE, vc.getConnectionMetaDataProviderTemplate());
+			createTemplateClass("TIQConnection", DEFAULT_PACKAGE, vc.getConnectionTemp());
+			createTemplateClass("TIQStatement", DEFAULT_PACKAGE, vc.getStatementTemp());
+			createdSources = true;
+			msgr.printMessage(Kind.NOTE, "Source generation successfully finished!");
+		}
+	}
+	
+	private void evalDriverAnnotatedElements(Set<? extends Element> driverAnnotatedElements) throws IOException {
+		int countOfAnnotatedClasses = driverAnnotatedElements.size();
+		if(countOfAnnotatedClasses == 1 && !createdDriverClass){
+			buildDriverClass(driverAnnotatedElements);
+		} else {
+			printWarning(countOfAnnotatedClasses);
+		}
+	}
+
+	private void printWarning(int countOfAnnotatedClasses) {
+		if(countOfAnnotatedClasses != 1){
+			msgr.printMessage(Kind.WARNING, "Driver Annotation has not been used in the right way! Occurence was: " + countOfAnnotatedClasses + " times" );
+		}
+	}
+
+	private void buildDriverClass(Set<? extends Element> driverAnnotatedElements) throws IOException {
+		Element annotatedType = driverAnnotatedElements.iterator().next();
+		JdbcDriver driverMetaInfo = annotatedType.getAnnotation(JdbcDriver.class);
+		String packageName = evaluatePackage(driverMetaInfo.packageDefinition(), annotatedType);
+		createDriverClass(packageName, driverMetaInfo);
+		executorClassName = annotatedType.toString();
+		createdDriverClass = true;
+	}
+	
+	private void getTypeNameOfConnectionAnnotatedElement(Set<? extends Element> connectionAnnotatedElements) {
+		if (!connectionAnnotatedElements.isEmpty()) {
+			Element conTypeName = connectionAnnotatedElements.iterator().next();
+			connectionHandlerClassName = conTypeName.toString();
+		} else {
+			msgr.printMessage(Kind.WARNING, "No ConnectionHandler class was found! Do you have set the @Connection annotation?");
+		}
+	}
+
+	private void getTypeNameOfConnectionMetaDataAnnotatedElement(Set<? extends Element> conMetaDataAnnotatedElements) {
+		if (!conMetaDataAnnotatedElements.isEmpty()) {
+			Element conMetaDataTypeName = conMetaDataAnnotatedElements.iterator().next();
+			connectionMetaDataProviderClassName = conMetaDataTypeName.toString();
+		} else {
+			msgr.printMessage(Kind.WARNING, "No ConnectionMetaData class was found!");
 		}
 	}
 
@@ -144,16 +159,6 @@ public class JdbcAnnotationProcessor extends AbstractProcessor{
 		return packageName.equals("") ? extractPackage(curElement.toString()) : packageName;
 	}
 
-	private boolean checkExecutorClass(TypeMirror superclass, String className) {
-		return superclass.toString().endsWith(className);
-	}
-
-	private void warnForUsage(Set<? extends Element> annotations, int driverAnnotationCount) {
-		driverAnnotationCount += annotations.size();
-		if (driverAnnotationCount != 1)
-			msgr.printMessage(Kind.WARNING, "Driver Annotation has not been used in the right way! Occurence was: " + this.driverAnnotationCount + " times" );
-	}
-	
 	private VelocityContext initializeVelocityContext() {
 		VelocityContext vcon = new VelocityContext();
 		vcon.put("creatingTimestamp",new SimpleDateFormat("dd.MM.yyyy HH:mm").format(new Date()));
@@ -169,13 +174,13 @@ public class JdbcAnnotationProcessor extends AbstractProcessor{
 		return file.openWriter();
 	}
 	
-	private void createDriverClass(String packageName, String className, String prefix, String scheme) throws IOException {
+	private void createDriverClass(String packageName, JdbcDriver driverMetaInfo) throws IOException {
 		VelocityContext vcon = initializeVelocityContext();
 		vcon.put("package", packageName);
-		vcon.put("className", className);
-		vcon.put("urlPrefix", prefix);
-		vcon.put("urlScheme", scheme);
-		Writer writer = createJavaSourceFile(className, packageName);
+		vcon.put("className", driverMetaInfo.name());
+		vcon.put("urlPrefix", driverMetaInfo.prefix());
+		vcon.put("urlScheme", driverMetaInfo.scheme());
+		Writer writer = createJavaSourceFile(driverMetaInfo.name(), packageName);
 		vc.createFileFromTemplate(vcon, vc.getDriverTemp(), writer);
 		writer.close();
 	}
@@ -195,7 +200,7 @@ public class JdbcAnnotationProcessor extends AbstractProcessor{
 		return StringUtils.join(Arrays.copyOfRange(parts,0,parts.length-cuttingDepth),".");
 	}
 
-	boolean foreachToStringEndsWith(Object[] testable, String searchable) {
+	boolean elementOfArrayEndsWith(Object[] testable, String searchable) {
 		for(Object obj : testable){
 			if(obj.toString().endsWith(searchable)){
 				return true;
---
+++ src/main/java/de/tiq/processor/data/AnnotationElementData.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2012, 2013 All Rights Reserved, www.tiq-solutions.com
+ * 
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This code is product of:
+ *
+ * TIQ Solutions GmbH 
+ * Weißenfelser Str. 84
+ * 04229 Leipzig, Germany
+ *
+ * info@tiq-solutions.com
+ *
+ */
+package de.tiq.processor.data;
+
+import java.util.Set;
+
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.Element;
+
+import de.tiq.jdbc.annotations.Connection;
+import de.tiq.jdbc.annotations.ConnectionMetaData;
+import de.tiq.jdbc.annotations.JdbcDriver;
+
+
+public class AnnotationElementData {
+	
+	Set<? extends Element> driverAnnotatedElements;
+	Set<? extends Element> connectionAnnotatedElements;
+	Set<? extends Element> conMetaDataAnnotatedElements;
+	
+	public AnnotationElementData(RoundEnvironment roundEnv) {
+		driverAnnotatedElements = roundEnv.getElementsAnnotatedWith(JdbcDriver.class);
+		connectionAnnotatedElements = roundEnv.getElementsAnnotatedWith(Connection.class);
+		conMetaDataAnnotatedElements = roundEnv.getElementsAnnotatedWith(ConnectionMetaData.class);
+	}
+	
+	public Set<? extends Element> getDriverAnnotatedElements() {
+		return driverAnnotatedElements;
+	}
+	public void setDriverAnnotatedElements(Set<? extends Element> driverAnnotatedElements) {
+		this.driverAnnotatedElements = driverAnnotatedElements;
+	}
+	public Set<? extends Element> getConnectionAnnotatedElements() {
+		return connectionAnnotatedElements;
+	}
+	public void setConnectionAnnotatedElements(Set<? extends Element> connectionAnnotatedElements) {
+		this.connectionAnnotatedElements = connectionAnnotatedElements;
+	}
+	public Set<? extends Element> getConMetaDataAnnotatedElements() {
+		return conMetaDataAnnotatedElements;
+	}
+	public void setConMetaDataAnnotatedElements(Set<? extends Element> conMetaDataAnnotatedElements) {
+		this.conMetaDataAnnotatedElements = conMetaDataAnnotatedElements;
+	}
+}
